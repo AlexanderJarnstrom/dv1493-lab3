aee835f770891ab9ad6753f9cf734d868bffeb0c
AlexanderJarnstrom <alexander.jarnstrom@gmail.com>
2024-05-05 14:37:56 +0200

    GetInt GetInt gets the number from  the buffer.\nMight need check if '-' is after the number.

--- lab3_lib.s
+++ lab3_lib.s
@@ -1,5 +1,6 @@
   .data
 buf: .space 20 
+pos: .word 0
   .text
   .global main
 
@@ -37,6 +38,7 @@ inImage:
   movq $20, %rsi
   movq stdin, %rdx
   call fgets
+  movw $0, pos
 
   popq %r11
   popq %r10
@@ -54,43 +56,69 @@ getInt:
   pushq %r8
   pushq %rbx
   pushq %rdx
-  leaq buf, %rbx
+  pushq %rsi
+  pushq %rdi
+  pushq %r9
+  movq $0, %rdi
+  movq $0, %rdx
+  movzwq pos, %rsi
 getIntLoop:
-  movb (%rbx), %r8b
-  incq %rbx
+  leaq buf, %rbx    # rbx = buf
+  addq %rsi, %rbx   # rbx += pos
+  movb (%rbx), %r8b # r8b = *rbx
+  incq %rsi
 
-  cmpb $0x0, %r8b
-  je getIntReturn
+  cmpb $0x0, %r8b   # r8b == 0x0
+  je getIntBuffEmpty
 
   cmpb $0x2b, %r8b  # r8b == +
-  je getIntPos
+  je getIntLoop
 
-  cmpb $0x2d, %r8b
+  cmpb $0x2d, %r8b  # r8b == -
   je getIntNeg
 
-  cmpb $0x30, %r8b
-  jl getIntReturn
-  cmpb $0x39, %r8b
-  jg getIntReturn
+  cmpb $0x30, %r8b  # r8b < 0x30
+  jl getIntLetter
+
+  cmpb $0x39, %r8b  # r8b > 0x39
+  jg getIntLetter
 
 getIntNum:
-  movq $1, %rdx
+  orb $0x2, %dil    # dil | 0b0000 0010
+  subb $0x30, %r8b  # r8b - 0x30
+  imulq $10, %rdx   # rdx * 10
+  movsbq %r8b, %r8  # r8b -> r8
+  addq %r8, %rdx    # rdx + r8
   jmp getIntLoop
 
-getIntPos:
-  movq $2, %rdx
-  jmp getIntLoop
-  
+getIntLetter:
+  testb $0x2, %dil  # dil & 0b0000 0010
+  je getIntLoop
+  testb $0x1, %dil  # dil & 0b0000 0001
+  je getIntNotNeg
+  imulq $-1, %rdx   # Negate
+getIntNotNeg:
+  jmp getIntReturn
+
 getIntNeg:
-  movq $3, %rdx
+  orb $0x1, %dil    # dil | 0b0000 0001
+  jmp getIntLoop
+
+getIntBuffEmpty:
+  call inImage
+  movzwq pos, %rsi
   jmp getIntLoop
 
 getIntReturn: 
+  movw %si, pos
+  movq %rdx, %rax
+  popq %r9
+  popq %rdi
+  popq %rsi
   popq %rdx
   popq %rbx
   popq %r8
   ret
-
 
 getText:
 getChar:
